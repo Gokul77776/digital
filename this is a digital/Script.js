fb3063db1aea77db2e37373d861770b73dd722b0
Gokul77776 <165793921+Gokul77776@users.noreply.github.com>
2024-04-03 05:00:41 +0530

    Add files via upload

---
+++ this is a digital/Script.js
@@ -0,0 +1,23 @@
+gsap.from("#one",{
+    delay:0.4,
+    opacity:0,
+    duration:1,
+    y:60
+})
+gsap.from("#two",{
+    delay:0.4,
+    opacity:0,
+    duration:1,
+    x:60
+})
+gsap.from("#three",{
+    delay:0.4,
+    opacity:0,
+    duration:1,
+    x:-60
+})
+gsap.from("#main h1",{
+    delay:0.4,
+    opacity:0,
+    duration:1,
+})
